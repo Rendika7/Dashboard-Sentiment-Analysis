adc34399c83a66ca57368999e4c5519ea1256733
Rendika Nurhartanto Suharto <72683810+Rendika7@users.noreply.github.com>
2024-01-01 04:50:27 +0700

    Update dashboard_second_choice.py

--- dashboard_second_choice.py
+++ dashboard_second_choice.py
@@ -75,7 +75,7 @@ st.sidebar.markdown("""
         }
     </style>
 """, unsafe_allow_html=True)
-st.sidebar.image('Source\TELKOM-UNIVERSITY-LOGO-HORIZONTAL.png', caption='Tel-U Surabaya',width=150 , use_column_width=False)  
+st.sidebar.image('https://surabaya.telkomuniversity.ac.id/wp-content/uploads/2023/09/TELKOM-UNIVERSITY-LOGO-HORIZONTAL.png', caption='Tel-U Surabaya',width=150 , use_column_width=False)  
 
 # ================================================================================================================
 
